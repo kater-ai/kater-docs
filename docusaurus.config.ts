a7cb7262b34d5875a508b0f366a116d0b2066b78
Robin Seitz <robinseitz@hotmail.com>
2024-01-02 20:56:26 -0800

    Bugfig(docs): Fix build error in github action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,9 +26,10 @@ const config: Config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       {
         docs: {
+          path: 'doc',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
