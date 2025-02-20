4dbd14b4b952213c2b94d1360b919e60a82bd23d
yvonne-chou-1 <yvonne@kater.ai>
2025-02-19 17:33:06 -0800

    added algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,38 +54,36 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // algolia: {
-    //   // The application ID provided by Algolia
-    //   appId: 'YOUR_APP_ID',
+    algolia: {
+      // The application ID provided by Algolia
+      appId: '38BWQ7QUKE',
 
-    //   // Public API key: it is safe to commit it
-    //   apiKey: 'YOUR_SEARCH_API_KEY',
+      // Public API key: it is safe to commit it
+      apiKey: 'ac9011af37bc27e95d9f4d3248ec9142',
 
-    //   indexName: 'YOUR_INDEX_NAME',
+      indexName: 'docs',
 
-    //   // Optional: see doc section below
-    //   contextualSearch: true,
+      // Optional: see doc section below
+      contextualSearch: true,
 
-    //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-    //   externalUrlRegex: 'external\\.com|domain\\.com',
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
 
-    //   // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-    //   replaceSearchResultPathname: {
-    //     from: '/docs/', // or as RegExp: /\/docs\//
-    //     to: '/',
-    //   },
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
 
-    //   // Optional: Algolia search parameters
-    //   searchParameters: {},
+      // Optional: Algolia search parameters
+      searchParameters: {},
 
-    //   // Optional: path for search page that enabled by default (`false` to disable it)
-    //   searchPagePath: 'search',
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
 
-    //   // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-    //   insights: false,
-
-    //   //... other Algolia params
-    // },
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+    },
     zoom: {
       // CSS selector to apply the plugin to, defaults to '.markdown img'
       selector: 'img:not(.navbar img):not(.footer img)',
